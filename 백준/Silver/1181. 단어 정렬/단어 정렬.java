4c8f6def1a4551c36237c21580606fc59229ec25
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2022-12-02 17:47:37 +0900

    [Silver V] Title: 단어 정렬, Time: 584 ms, Memory: 21968 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Set<String> arr = new HashSet<>();
+
+        int loop = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < loop; i++) {
+            arr.add(br.readLine());
+        }
+
+        List<String> setArr = new ArrayList<>(List.copyOf(arr));
+
+        setArr.sort(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if (o1.length() > o2.length()) {
+                    return 1;
+                } else if (o1.length() == o2.length()) {
+                    return o1.compareTo(o2);
+                } else {
+                    return -1;
+                }
+            }
+        });
+
+        for (String s : setArr) {
+            System.out.println(s);
+        }
+    }
+}
