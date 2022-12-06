15655b86a97fc805941333fdf23404f0bdf19e31
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2022-12-06 10:23:00 +0900

    [Bronze I] Title: 최소공배수, Time: 188 ms, Memory: 15024 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int loop = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < loop; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            int result = gcd(a, b);
+
+            System.out.println(a * b / result);
+        }
+    }
+
+    static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return gcd(b, a % b);
+        }
+    }
+}
