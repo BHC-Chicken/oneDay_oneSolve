21bea41cc0363e98989981c99e42513fed0b2ab6
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2022-12-16 11:36:48 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1452 ms, Memory: 129884 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        
+        int loop = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < loop; i++) {
+            arrayList.add(Integer.parseInt(br.readLine()));
+        }
+
+        Collections.sort(arrayList);
+
+
+        for (int i : arrayList) {
+            sb.append(i).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
