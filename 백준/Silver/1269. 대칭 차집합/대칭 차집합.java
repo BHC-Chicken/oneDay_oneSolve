efe8fb18972025a2e439906c81da235a68d75e15
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2022-12-28 18:04:55 +0900

    [Silver IV] Title: 대칭 차집합, Time: 740 ms, Memory: 83956 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
@@ -2,11 +2,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.StringTokenizer;
 
 public class Main {
-    int count = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,33 +12,25 @@ public class Main {
 
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
-        int count = 0;
 
-        HashSet<Integer> set1 = new HashSet<>();
-        HashSet<Integer> set2 = new HashSet<>();
+        HashSet<Integer> set = new HashSet<>();
 
         st = new StringTokenizer(br.readLine());
         while (st.hasMoreTokens()) {
-            set1.add(Integer.parseInt(st.nextToken()));
+            set.add(Integer.parseInt(st.nextToken()));
         }
 
         st = new StringTokenizer(br.readLine());
+
         while (st.hasMoreTokens()) {
-            set2.add(Integer.parseInt(st.nextToken()));
-        }
-
-        for (Integer integer : set2) {
-            if (!set1.contains(integer)) {
-                count++;
+            int num = Integer.parseInt(st.nextToken());
+            if (set.contains(num)) {
+                set.remove(num);
+            } else {
+                set.add(num);
             }
         }
 
-        for (Integer integer : set1) {
-            if (!set2.contains(integer)) {
-                count++;
-            }
-        }
-
-        System.out.println(count);
+        System.out.println(set.size());
     }
 }
