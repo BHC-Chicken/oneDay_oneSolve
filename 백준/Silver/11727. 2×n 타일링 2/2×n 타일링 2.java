0c0824153b152f221722abf142a2987f3d41c80c
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2023-01-13 01:20:20 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 132 ms, Memory: 14376 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        long[] arr = new long[1001];
+
+        arr[0] = 0;
+        arr[1] = 1;
+        arr[2] = 3;
+        arr[3] = 5;
+
+        for (int i = 4; i <= num; i++) {
+            arr[i] = ((arr[i - 2] * 2) + arr[i - 1]) % 10007;
+        }
+
+        System.out.println(arr[num]);
+    }
+}
