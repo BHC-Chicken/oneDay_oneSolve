cb74e6f7375874aac18a5858ae650ed4e19f5a16
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2023-03-03 00:52:29 +0900

    [Silver III] Title: N과 M (3), Time: 452 ms, Memory: 68548 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static int n;
+    static int m;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[m];
+
+        dfs( 0);
+
+        System.out.println(sb);
+    }
+
+    static void dfs(int num) {
+        if (num == m) {
+            for (int b : arr) {
+                sb.append(b).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            arr[num] = i;
+            dfs(num + 1);
+        }
+    }
+}
