55aa957ed548146c414a89e2f571eb9fb15dd0b7
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2023-03-15 14:01:38 +0900

    [Silver III] Title: 수리공 항승, Time: 124 ms, Memory: 14344 KB -BaekjoonHub

---
+++ 백준/Silver/1449. 수리공 항승/수리공 항승.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int count = 1;
+
+        int[] arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        double left = arr[0] - 0.5;
+        for (int i = 0; i < n; i++) {
+            if (left + m < arr[i]) {
+                count++;
+                left = arr[i] - 0.5;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
