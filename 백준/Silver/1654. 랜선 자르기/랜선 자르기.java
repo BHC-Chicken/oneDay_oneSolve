9e23230be78fdd08723c18289143223e34732f19
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2023-03-23 17:53:13 +0900

    [Silver II] Title: 랜선 자르기, Time: 184 ms, Memory: 16868 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int num = Integer.parseInt(st.nextToken());
+        int value = Integer.parseInt(st.nextToken());
+        long max = 0;
+        int[] arr= new int[num];
+
+        for (int i = 0; i < num; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            if (max < arr[i]) {
+                max = arr[i];
+            }
+        }
+
+        max++;
+
+        long min = 0;
+        long mid = 0;
+
+        while (min < max) {
+            mid = (min + max) / 2;
+            long count = 0;
+
+            for (int i = 0; i < arr.length; i++) {
+                count += arr[i] / mid;
+            }
+
+            if (count < value) {
+                max = mid;
+            } else {
+                min = mid + 1;
+            }
+        }
+
+        System.out.println(min - 1);
+
+    }
+
+}
