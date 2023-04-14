0a982879947bc79dd7a4c3ee14da6fd051620b08
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2023-04-14 23:22:21 +0900

    [Silver III] Title: 문자열 집합, Time: 420 ms, Memory: 31088 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -1,6 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -13,14 +13,14 @@ public class Main {
         int m = Integer.parseInt(st.nextToken());
         int count = 0;
 
-        ArrayList<String> s = new ArrayList<>();
+        HashSet<String> hashSet = new HashSet<>();
 
         for (int i = 0; i < n; i++) {
-            s.add(br.readLine());
+            hashSet.add(br.readLine());
         }
 
         for (int i = 0; i < m; i++) {
-            if (s.contains(br.readLine())) {
+            if (hashSet.contains(br.readLine())) {
                 count++;
             }
         }
