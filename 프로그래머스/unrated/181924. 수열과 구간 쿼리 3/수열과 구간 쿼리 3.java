c6d3111af677a62c8ec8b620cbb48eb5b580c00f
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2023-06-07 22:06:21 +0900

    [unrated] Title: 수열과 구간 쿼리 3, Time: 0.04 ms, Memory: 79.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+    
+        for (int i = 0; i < queries.length; i++) {
+            int num1 = queries[i][0];
+            int num2 = queries[i][1];
+            
+            int temp = arr[num2];
+            arr[num2] = arr[num1];
+            arr[num1] = temp;
+        }
+        return arr;
+    }
+}
