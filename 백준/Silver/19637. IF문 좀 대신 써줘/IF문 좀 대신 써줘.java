8b1a9a8d8afe60447550186be1b932dff8c21e2a
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2023-07-03 21:57:43 +0900

    [Silver III] Title: IF문 좀 대신 써줘, Time: 660 ms, Memory: 56760 KB -BaekjoonHub

---
+++ 백준/Silver/19637. IF문 좀 대신 써줘/IF문 좀 대신 써줘.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        String[][] title = new String[n][2];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            title[i][0] = st.nextToken();
+            title[i][1] = st.nextToken();
+        }
+
+        for (int i = 0; i < m; i++) {
+            int num = Integer.parseInt(br.readLine());
+
+            int left = 0;
+            int right = n - 1;
+            int mid;
+
+            while (left <= right) {
+                mid = (left + right) / 2;
+
+                int titleInt = Integer.parseInt(title[mid][1]);
+                if (titleInt < num) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+
+            sb.append(title[left][0]).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
