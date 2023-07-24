57dd12e156223d247c5fbc9c03df53aba4ba53a9
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2023-07-24 23:38:08 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 사칙연산, Time: 132 ms, Memory: 26,584 KB -BaekjoonHub

---
+++ SWEA/D4/1232. ［S／W 문제해결 기본］ 9일차 － 사칙연산/［S／W 문제해결 기본］ 9일차 － 사칙연산.java
@@ -0,0 +1,85 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+class Solution {
+    static int[][] tree;
+    static String[] value;
+    static Stack<Double> stack;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        for(int test_case = 1; test_case <= 10; test_case++) {
+            int N = Integer.parseInt(br.readLine());
+
+            tree = new int[N+1][2];
+            value = new String[N+1];
+
+            for(int i = 1; i <= N; i++) {
+                String[] arr = br.readLine().split(" ");
+
+                value[i] = arr[1];
+                stack = new Stack<>();
+
+                if(arr.length == 4) {
+                    int a = Integer.parseInt(arr[0]);
+                    int b = Integer.parseInt(arr[2]);
+                    int c = Integer.parseInt(arr[3]);
+
+                    tree[a][0] = b;
+                    tree[a][1] = c;
+                }
+            }
+
+            postOrder(1);
+
+            int result = stack.pop().intValue();
+            sb.append("#").append(test_case).append(" ").append(result).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    static void postOrder(int x) {
+        if (tree[x][0] != 0 || tree[x][1] != 0) {
+            if (tree[x][0] != 0) {
+                postOrder(tree[x][0]);
+            }
+
+            if (tree[x][1] != 0) {
+                postOrder(tree[x][1]);
+            }
+
+        }
+        operation(value[x]);
+    }
+
+    static void operation(String v) {
+        if(!v.equals("+") && !v.equals("-") && !v.equals("*") && !v.equals("/")) {
+            stack.push(Double.parseDouble(v));
+            return;
+        }
+
+        Double b = stack.pop();
+        Double a = stack.pop();
+
+        switch (v) {
+            case "+":
+                stack.push(a + b);
+                break;
+            case "-":
+                stack.push(a - b);
+                break;
+            case "*":
+                stack.push(a * b);
+                break;
+            case "/":
+                stack.push(a / b);
+                break;
+        }
+    }
+
+}
