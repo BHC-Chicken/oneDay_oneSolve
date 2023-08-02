f29cc05bdbdf084b23e09538c111023aa9123484
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2023-08-02 11:57:04 +0900

    [Silver III] Title: 돌 게임 3, Time: 136 ms, Memory: 14288 KB -BaekjoonHub

---
+++ 백준/Silver/9657. 돌 게임 3/돌 게임 3.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int num = Integer.parseInt(br.readLine());
+        int[] dp = new int[1001];
+
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 1;
+        dp[4] = 1;
+
+        for (int i = 5; i <= num; i++) {
+            dp[i] = 2;
+
+            if (dp[i-1] % 2 == 0 || dp[i - 3] % 2 == 0 || dp[i-4] % 2 == 0) {
+                dp[i] = 1;
+            }
+        }
+
+        if (dp[num] == 1) {
+            sb.append("SK");
+        } else {
+            sb.append("CY");
+        }
+
+        System.out.println(sb);
+    }
+}
