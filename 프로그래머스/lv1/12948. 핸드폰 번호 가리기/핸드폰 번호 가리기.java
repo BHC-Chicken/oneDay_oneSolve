5c921e0d4912d95d615ad04fdd38d41eb13749ad
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2023-08-08 14:35:18 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.07 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String phone_number) {
+        StringBuilder sb = new StringBuilder(phone_number);
+        for (int i = 0; i < sb.length() - 4; i++) {
+            sb.replace(i, i+1, "*");
+        }
+        
+        return sb.toString();
+    }
+}
