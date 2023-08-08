948ed1b4ee2c4b0fdec24696942bb4620e7a24c4
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2023-08-08 09:29:52 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -0,0 +1,13 @@
+class Solution {
+    public long solution(long n) {
+        long answer = 0;
+        double temp = Math.sqrt(n);
+        
+        if(temp % 1 == 0) {
+            return (long) Math.pow((temp + 1), 2);
+        } else {
+            return -1;
+        }
+        
+    }
+}
