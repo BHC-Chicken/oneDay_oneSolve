28090d9fcc8bfe706638f585c172aff70ce4d739
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2023-08-08 14:22:27 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.08 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -2,8 +2,13 @@ import java.util.List;
 
 class Solution {
     public String solution(String[] seoul) {
-        List<String> list = List.of(seoul);
-        int index = list.indexOf("Kim");
+        int index = 0;
+        for (int i = 0; i < seoul.length; i++) {
+            if(seoul[i].equals("Kim")) {
+                index = i;
+                break;
+            }
+        }
         StringBuilder sb = new StringBuilder();
         sb.append("김서방은 ").append(index).append("에 있다");
 
