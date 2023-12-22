d59562de763d3a8852c531cc54b257ad154b0d75
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2023-12-22 21:06:42 +0900

    [Silver III] Title: 정수 a를 k로 만들기, Time: 120 ms, Memory: 14072 KB -BaekjoonHub

---
+++ 백준/Silver/25418. 정수 a를 k로 만들기/정수 a를 k로 만들기.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int count = 0;
+
+        while (a != k) {
+            if (k % 2 == 0 && k / 2 >= a) {
+                k /= 2;
+            } else {
+                k -= 1;
+            }
+
+            count++;
+        }
+
+        System.out.println(count);
+    }
+}
