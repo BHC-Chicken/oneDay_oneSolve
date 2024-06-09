890a966bb615ac12f8fe2332fc4e5454119a7e37
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2024-06-10 01:02:57 +0900

    [Silver IV] Title: 정수 제곱근, Time: 124 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Silver/2417. 정수 제곱근/정수 제곱근.java
+++ 백준/Silver/2417. 정수 제곱근/정수 제곱근.java
@@ -3,25 +3,30 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        long num = Long.parseLong(br.readLine());
+    static long binary(long num) {
         long start = 0;
         long end = num;
-        long min = Long.MIN_VALUE;
+        long result = 0;
 
         while (start <= end) {
             long mid = (start + end) / 2;
-            long result = (long) Math.pow(mid, 2);
-            
-            if (result >= num) {
-                min = mid;
+
+            if (num <= Math.pow(mid, 2)) {
+                result = mid;
                 end = mid - 1;
             } else {
                 start = mid + 1;
             }
         }
-        System.out.println(min);
+
+        return result;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        long num = Long.parseLong(br.readLine());
+
+        System.out.println(binary(num));
     }
 }
