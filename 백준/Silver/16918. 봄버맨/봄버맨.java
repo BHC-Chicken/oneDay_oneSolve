59ae1b9e8c0d84f4a162e24b32822e8991ef40c2
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2024-06-24 19:19:37 +0900

    [Silver I] Title: 봄버맨, Time: 156 ms, Memory: 15068 KB -BaekjoonHub

--- 백준/Silver/16918. 봄버맨/봄버맨.java
+++ 백준/Silver/16918. 봄버맨/봄버맨.java
@@ -1,100 +1,100 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int r;
-    static int c;
-    static int n;
-    static String[][] arr;
-    static boolean[][] bomb;
-    static int[] dx = {1, 0, -1, 0};
-    static int[] dy = {0, -1, 0, 1};
 
-    static boolean inRange(int x, int y) {
-        return x >= 0 && x < r && y >= 0 && y < c;
+    static int row;
+    static int col;
+    static int N;
+
+    static char[][] curMap;
+    static char[][] nMap;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder res = new StringBuilder();
+
+        row = Integer.parseInt(st.nextToken());
+        col = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+
+        curMap = new char[row][col];
+        nMap = new char[row][col];
+
+        /* map init */
+        for(int i = 0; i < row; i++) {
+            char[] c = br.readLine().toCharArray();
+            for(int j = 0; j < col; j++) {
+                curMap[i][j] = c[j];
+            }
+        }
+
+        if (N == 1) {
+            // Initial state remains unchanged
+            printMap(curMap);
+        } else if (N % 2 == 0) {
+            // All cells are filled with bombs
+            fillMapWithBombs();
+            printMap(nMap);
+        } else {
+            // Odd N, we need to simulate 3 seconds or 5 seconds
+            simulate(3);
+            if (N % 4 == 3) {
+                printMap(nMap);
+            } else {
+                // Simulate further to 5 seconds state (next 3-second state after initial)
+                char[][] tempMap = copyMap(nMap);
+                curMap = tempMap;
+                simulate(3);
+                printMap(nMap);
+            }
+        }
     }
 
-    static void explosion() {
-        bomb = new boolean[r][c];
-
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                if (arr[i][j].equals("O")) {
-                    bomb[i][j] = true;
-
-                    for (int k = 0; k < 4; k++) {
-                        if (inRange(i + dx[k], j + dy[k])) {
-                            bomb[i + dx[k]][j + dy[k]] = true;
-                        }
+    static void simulate(int seconds) {
+        for (int i = 0; i < seconds; i++) {
+            fillMapWithBombs();
+            for (int r = 0; r < row; r++) {
+                for (int c = 0; c < col; c++) {
+                    if (curMap[r][c] == 'O') {
+                        nMap[r][c] = '.';
+                        if (r - 1 >= 0) nMap[r - 1][c] = '.';
+                        if (r + 1 < row) nMap[r + 1][c] = '.';
+                        if (c - 1 >= 0) nMap[r][c - 1] = '.';
+                        if (c + 1 < col) nMap[r][c + 1] = '.';
                     }
                 }
             }
+            curMap = copyMap(nMap);
         }
     }
 
-    static void location() {
-        arr = new String[r][c];
-
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                if (bomb[i][j]) {
-                    arr[i][j] = ".";
-                } else {
-                    arr[i][j] = "O";
-                }
+    static void fillMapWithBombs() {
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                nMap[i][j] = 'O';
             }
         }
     }
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
-
-        r = Integer.parseInt(st.nextToken());
-        c = Integer.parseInt(st.nextToken());
-        n = Integer.parseInt(st.nextToken());
-
-        arr = new String[r][c];
-        bomb = new boolean[r][c];
-
-        if (n % 2 == 0) {
-            for (int i = 0; i < r; i++) {
-                sb.append("O".repeat(Math.max(0, c)));
-                sb.append("\n");
-            }
-
-            System.out.println(sb);
-
-            return;
+    static char[][] copyMap(char[][] map) {
+        char[][] newMap = new char[row][col];
+        for (int i = 0; i < row; i++) {
+            System.arraycopy(map[i], 0, newMap[i], 0, col);
         }
+        return newMap;
+    }
 
-        for (int i = 0; i < r; i++) {
-            String[] split = br.readLine().split("");
-            for (int j = 0; j < c; j++) {
-                arr[i][j] = split[j];
-                bomb[i][j] = !split[j].equals(".");
+    static void printMap(char[][] map) {
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                res.append(map[i][j]);
             }
+            res.append('\n');
         }
-
-        for (int i = 2; i <= n; i++) {
-            if (i % 2 == 0) {
-                continue;
-            }
-
-            explosion();
-            location();
-        }
-
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                sb.append(arr[i][j]);
-            }
-            sb.append("\n");
-        }
-
-        System.out.println(sb);
+        System.out.print(res);
     }
 }
