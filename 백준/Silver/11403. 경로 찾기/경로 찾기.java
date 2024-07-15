95d1f0837cae161fdd9ac9099aead94cad0ee134
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2024-07-16 01:12:21 +0900

    [Silver I] Title: 경로 찾기, Time: 164 ms, Memory: 16456 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            String[] line = br.readLine().split(" ");
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = Integer.parseInt(line[j]);
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+                    if (arr[j][i] == 1 && arr[i][k] == 1) {
+                        arr[j][k] = 1;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                sb.append(arr[i][j]).append(" ");
+            }
+
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
