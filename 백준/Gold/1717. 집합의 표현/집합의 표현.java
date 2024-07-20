1b2d68297873bdd080f75e9df44a3887bed6df06
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2024-07-21 04:12:24 +0900

    [Gold V] Title: 집합의 표현, Time: 752 ms, Memory: 62040 KB -BaekjoonHub

---
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -0,0 +1,74 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] arr;
+
+    static int find(int x) {
+        if (x == arr[x]) {
+            return x;
+        }
+
+        return arr[x] = find(arr[x]);
+    }
+
+    static void union(int x, int y) {
+        x = find(x);
+        y = find(y);
+
+        if (x != y) {
+            if (x < y) {
+                arr[y] = x;
+            } else {
+                arr[x] = y;
+            }
+        }
+    }
+
+    static boolean isSameNum(int x, int y) {
+        x = find(x);
+        y = find(y);
+
+        return x == y;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            arr[i] = i;
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int cmd = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if (cmd == 0) {
+                union(a,b);
+            } else if (cmd == 1) {
+                if (isSameNum(a,b)) {
+                    sb.append("YES");
+                } else {
+                    sb.append("NO");
+                }
+
+                sb.append("\n");
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
+
