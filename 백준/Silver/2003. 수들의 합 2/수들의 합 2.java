1bb16661fd604247b9eb9735506926a3463c3e7a
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2024-08-01 01:27:27 +0900

    [Silver IV] Title: 수들의 합 2, Time: 152 ms, Memory: 14920 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -4,38 +4,40 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int loop = Integer.parseInt(st.nextToken());
-        int count = 0;
 
-        int[] arr = new int[loop];
-        int num = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n + 1];
+        arr[0] = 0;
 
         st = new StringTokenizer(br.readLine());
 
-        for (int i = 0; i < loop; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken()) + arr[i - 1];
         }
 
-        int a = 0;
-        for (int i = 0; i < loop; i++) {
-            int sum = 0;
-            for (int temp = a; temp < loop; temp++) {
-                sum += arr[temp];
-                if (sum == num) {
-                    count++;
-                    a++;
-                    break;
-                }
-                if (sum > num) {
-                    a++;
-                    break;
-                }
+        int left = 0;
+        int right = 1;
+        int result = 0;
+
+        while (left != n) {
+            if (arr[right] - arr[left] == m) {
+                result++;
+                right++;
+            } else if (arr[right] - arr[left] <= m) {
+                right++;
+            } else {
+                left++;
+            }
+            if (right > n) {
+                break;
             }
         }
-        System.out.println(count);
+
+        System.out.println(result);
     }
 }
