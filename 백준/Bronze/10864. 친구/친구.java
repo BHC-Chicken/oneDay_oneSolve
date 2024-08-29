12c72c76b5d661e545876c86b34979aee1d116c3
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2024-08-29 18:13:10 +0900

    [Bronze II] Title: 친구, Time: 112 ms, Memory: 14852 KB -BaekjoonHub

---
+++ 백준/Bronze/10864. 친구/친구.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] map = new int[n + 1];
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            map[a]++;
+            map[b]++;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            sb.append(map[i]).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
