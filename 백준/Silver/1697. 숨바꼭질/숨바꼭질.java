38394309e2bbfa6b14de9323ce5a82b47063da57
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2024-10-08 22:06:10 +0900

    [Silver I] Title: 숨바꼭질, Time: 136 ms, Memory: 18520 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -10,6 +10,38 @@ public class Main {
     static int k;
     static int[] visited = new int[100001];
 
+    static int bfs() {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(n);
+        visited[n] = 1;
+
+        while (!queue.isEmpty()) {
+            int num = queue.poll();
+
+            if (num == k) {
+
+                return visited[num] - 1;
+            }
+
+            if (num - 1 >= 0 && visited[num - 1] == 0) {
+                queue.add(num - 1);
+                visited[num - 1] = visited[num] + 1;
+            }
+
+            if (num + 1 <= 100000 && visited[num + 1] == 0) {
+                queue.add(num + 1);
+                visited[num + 1] = visited[num] + 1;
+            }
+
+            if (num * 2 <= 100000 && visited[num * 2] == 0) {
+                queue.add(num * 2);
+                visited[num * 2] = visited[num] + 1;
+            }
+        }
+
+        return 0;
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -17,36 +49,6 @@ public class Main {
         n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
 
-        System.out.println(bfs(n));
-    }
-
-    static int bfs(int num) {
-        Queue<Integer> q = new LinkedList<>();
-
-        q.add(num);
-        int temp = 0;
-        visited[num] = 1;
-
-        while (!q.isEmpty()) {
-            temp = q.poll();
-
-            if (temp == k) {
-                return visited[temp] - 1;
-            }
-            if (temp - 1 >= 0 && visited[temp - 1] == 0) {
-                visited[temp - 1] = visited[temp] + 1;
-                q.add(temp - 1);
-            }
-            if (temp + 1 <= 100000 && visited[temp + 1] == 0) {
-                visited[temp + 1] = visited[temp] + 1;
-                q.add(temp + 1);
-            }
-            if (2 * temp <= 100000 && visited[2 * temp] == 0) {
-                visited[2 * temp] = visited[temp] + 1;
-                q.add(2 * temp);
-            }
-        }
-
-        return -1;
+        System.out.println(bfs());
     }
 }
