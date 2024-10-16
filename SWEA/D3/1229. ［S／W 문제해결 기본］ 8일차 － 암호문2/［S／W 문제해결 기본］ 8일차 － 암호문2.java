dea49f8a41eccfd3654df8fa26ddcfd41f341e11
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2024-10-17 00:32:13 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문2, Time: 111 ms, Memory: 19,656 KB -BaekjoonHub

---
+++ SWEA/D3/1229. ［S／W 문제해결 기본］ 8일차 － 암호문2/［S／W 문제해결 기본］ 8일차 － 암호문2.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        for (int tc = 1; tc <= 10; tc++) {
+            int n = Integer.parseInt(br.readLine());
+            ArrayList<Integer> list = new ArrayList<>();
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < n; i++) {
+                list.add(Integer.parseInt(st.nextToken()));
+            }
+
+            int num = Integer.parseInt(br.readLine());
+
+            st = new StringTokenizer(br.readLine());
+
+            for (int i = 0; i < num; i++) {
+                String cmd = st.nextToken();
+
+                if (cmd.equals("I")) {
+                    int start = Integer.parseInt(st.nextToken());
+                    int end = Integer.parseInt(st.nextToken());
+
+                    for (int j = 0; j < end; j++) {
+                        list.add(start, Integer.parseInt(st.nextToken()));
+
+                        start += 1;
+                    }
+                } else if (cmd.equals("D")) {
+                    int start = Integer.parseInt(st.nextToken());
+                    int end = Integer.parseInt(st.nextToken());
+
+                    for (int j = 0; j < end; j++) {
+                        list.remove(start);
+                    }
+                }
+            }
+            sb.append("#").append(tc).append(" ");
+
+            for (int i = 0; i < 10; i++) {
+                sb.append(list.get(i)).append(" ");
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
