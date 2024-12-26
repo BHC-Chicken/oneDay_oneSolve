83f739c95d0f7d0a41b59cd0dc5466a403d6ee73
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2024-12-27 01:15:17 +0900

    [Silver II] Title: A → B, Time: 104 ms, Memory: 14084 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.java
+++ 백준/Silver/16953. A → B/A → B.java
@@ -4,30 +4,30 @@ import java.io.InputStreamReader;
 
 public class Main {
     static int result = Integer.MAX_VALUE;
-    static String start;
-    static String goal;
+    static int start;
+    static int goal;
 
-    static void dfs(String num, int count) {
-        if (num.equals(goal)) {
+    static void dfs(long num, int count) {
+        if (num == goal) {
             result = Math.min(count, result);
 
             return;
         }
 
-        if (Long.parseLong(num) > Long.parseLong(goal)) {
+        if (num > goal) {
             return;
         }
 
-        dfs(String.valueOf(Long.parseLong(num) * 2), count + 1);
-        dfs(num + "1", count + 1);
+        dfs(num * 2, count + 1);
+        dfs(num * 10 + 1, count + 1);
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] numbers = br.readLine().split(" ");
 
-        start = numbers[0];
-        goal = numbers[1];
+        start = Integer.parseInt(numbers[0]);
+        goal = Integer.parseInt(numbers[1]);
 
         dfs(start, 1);
 
