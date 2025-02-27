5ed155e90bc9e008e2de681438e38e0eeb85ba0f
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2025-02-27 21:36:16 +0900

    [Silver I] Title: RGB거리, Time: 116 ms, Memory: 18416 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -4,32 +4,27 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
 
-        int N = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
-        int[][] Cost = new int[N][3];
+        int[][] cost = new int[n][n];
 
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
 
-            Cost[i][0] = Integer.parseInt(st.nextToken());
-            Cost[i][1] = Integer.parseInt(st.nextToken());
-            Cost[i][2] = Integer.parseInt(st.nextToken());
-
+            cost[i][0] = Integer.parseInt(st.nextToken());
+            cost[i][1] = Integer.parseInt(st.nextToken());
+            cost[i][2] = Integer.parseInt(st.nextToken());
         }
 
-        for (int i = 1; i < N; i++) {
-            Cost[i][0] += Math.min(Cost[i - 1][1], Cost[i - 1][2]);
-            Cost[i][1] += Math.min(Cost[i - 1][0], Cost[i - 1][2]);
-            Cost[i][2] += Math.min(Cost[i - 1][0], Cost[i - 1][1]);
+        for (int i = 1; i < n; i++) {
+            cost[i][0] += Math.min(cost[i - 1][1], cost[i - 1][2]);
+            cost[i][1] += Math.min(cost[i - 1][0], cost[i - 1][2]);
+            cost[i][2] += Math.min(cost[i - 1][1], cost[i - 1][0]);
         }
 
-        System.out.println(Math.min(Math.min(Cost[N - 1][0], Cost[N - 1][1]), Cost[N - 1][2]));
+        System.out.println(Math.min(cost[n - 1][0], Math.min(cost[n - 1][1], cost[n - 1][2])));
     }
-
 }
