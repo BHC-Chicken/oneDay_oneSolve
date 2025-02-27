7cd45027a2ead798190db2eac706af59f3ad4294
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2025-02-28 00:43:04 +0900

    [Silver IV] Title: 큐, Time: 164 ms, Memory: 19868 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            String cmd = st .nextToken();
+
+            switch (cmd) {
+                case "push":
+                    deque.add(Integer.parseInt(st.nextToken()));
+                    break;
+                case "pop":
+                    if (deque.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(deque.poll()).append("\n");
+                    }
+                    break;
+                case "size":
+                    sb.append(deque.size()).append("\n");
+                    break;
+                case "empty":
+                    if (deque.isEmpty()) {
+                        sb.append(1).append("\n");
+                    } else {
+                        sb.append(0).append("\n");
+                    }
+                    break;
+                case "front":
+                    if (deque.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(deque.peek()).append("\n");
+                    }
+                    break;
+                default:
+                    if (deque.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(deque.peekLast()).append("\n");
+                    }
+                    break;
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
