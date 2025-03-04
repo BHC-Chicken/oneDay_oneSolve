c4ff71e9639636f08415ff5cfc7f5b0cfd8448f9
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2025-03-05 02:00:52 +0900

    [Silver IV] Title: solved.ac, Time: 340 ms, Memory: 37740 KB -BaekjoonHub

---
+++ 백준/Silver/18110. solved．ac/solved．ac.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        if (n == 0) {
+            System.out.println(0);
+
+            return;
+        }
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        int m = (int) Math.round((float) n * 0.15);
+
+        double result = 0;
+
+        for (int i = m; i < n - m; i++) {
+            result += arr[i];
+        }
+
+        System.out.println(Math.round(result / (n - (m * 2))));
+    }
+}
