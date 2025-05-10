b5783be7d6aa53e17ffeb3e9d68d4d5c9c2962fa
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2025-05-10 20:46:48 +0900

    [Silver III] Title: N과 M (5), Time: 332 ms, Memory: 46092 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n;
+    static int m;
+    static int[] arr;
+    static int[] out;
+    static boolean[] visited;
+
+    static ArrayList<String> list = new ArrayList<>();
+
+    static void dfs(int depth) {
+        if (depth == m) {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < m; i++) {
+                sb.append(out[i]).append(" ");
+            }
+
+            list.add(sb.toString().trim());
+
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                out[depth] = arr[i];
+                dfs(depth + 1);
+                visited[i] = false;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+
+        arr = new int[n];
+        out = new int[m];
+        visited = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        dfs(0);
+
+        StringBuilder result = new StringBuilder();
+
+        for (String s : list) {
+            result.append(s).append("\n");
+        }
+
+        System.out.println(result);
+    }
+}
