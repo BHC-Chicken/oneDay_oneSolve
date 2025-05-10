d9bab63eee2c89351c7640a7c1a0d838cf67fafe
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2025-05-10 18:02:12 +0900

    [Silver I] Title: IOIOI, Time: 240 ms, Memory: 20184 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -6,28 +6,35 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int num = Integer.parseInt(br.readLine());
-        int length = Integer.parseInt(br.readLine());
-        int count = 0;
-
-        StringBuilder find = new StringBuilder("IOI");
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
         String word = br.readLine();
 
-        find.append("OI".repeat(Math.max(0, num - 1)));
+        int result = 0;
 
-        for (int i = 0; i < length; i++) {
+        for (int i = 0; i < m; i++) {
             if (word.charAt(i) == 'I') {
-                if (i + find.length() > word.length()) {
-                    break;
+                int temp = i;
+                int count = 0;
+
+                while (temp + 2 < m) {
+                    if (word.charAt(temp + 1) == 'O' && word.charAt(temp + 2) == 'I') {
+                        count++;
+                        temp += 2;
+
+                        if (count == n) {
+                            count --;
+                            result++;
+                        }
+                    } else {
+                        break;
+                    }
                 }
-                String temp = word.substring(i, i + find.length());
-                if (temp.equals(find.toString())) {
-                    count++;
-                    i++;
-                }
+
+                i = temp;
             }
         }
 
-        System.out.println(count);
+        System.out.println(result);
     }
 }
