fb70b43688e86279460c5bf43c992ff463565e6e
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2025-11-13 22:21:38 +0900

    [Gold IV] Title: 키 순서, Time: 596 ms, Memory: 37100 KB -BaekjoonHub

---
+++ 백준/Gold/2458. 키 순서/키 순서.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        boolean[][] canCompare = new boolean[n + 1][n + 1];
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            canCompare[a][b] = true;
+        }
+
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (canCompare[i][k] && canCompare[k][j]) {
+                        canCompare[i][j] = true;
+                    }
+                }
+            }
+        }
+
+        int answer = 0;
+
+        for (int i = 1; i <= n; i++) {
+            int count = 0;
+            for (int j = 1; j <= n; j++) {
+                if (i == j) {
+                    continue;
+                }
+
+                if (canCompare[i][j] || canCompare[j][i]) {
+                    count++;
+                }
+            }
+
+            if (count == n - 1) {
+                answer++;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
