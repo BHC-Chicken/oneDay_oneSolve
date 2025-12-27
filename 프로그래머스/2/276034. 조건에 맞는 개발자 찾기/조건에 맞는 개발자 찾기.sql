def1f6645b53165aacf3fc6b88c3a79b062d0a4a
PHC <57649545+BHC-Chicken@users.noreply.github.com>
2025-12-28 01:55:11 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -0,0 +1,5 @@
+select distinct d.ID, d.EMAIL, d.FIRST_NAME, d.LAST_NAME
+from developers as d
+join skillcodes as s on (d.skill_code & s.code) > 0
+where s.name in ('Python', 'C#')
+order by d.id asc;
